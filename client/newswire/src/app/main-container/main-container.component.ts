626bd43b6aca66dca83203957fcc1c833213f797
Iliyan Zafirov <iliyanzafirov00@gmail.com>
2024-03-07 12:51:32 +0200

    feat: article service fetches data from backend

--- client/newswire/src/app/main-container/main-container.component.ts
+++ client/newswire/src/app/main-container/main-container.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Article } from '../model/article.model';
+import { ArticleService } from '../article-service/article.service';
 
 @Component({
   selector: 'app-main-container',
@@ -7,6 +9,23 @@ import { Component } from '@angular/core';
   templateUrl: './main-container.component.html',
   styleUrl: './main-container.component.css'
 })
-export class MainContainerComponent {
+export class MainContainerComponent implements OnInit {
+  articles?: Article[];
 
+  constructor(private articleService: ArticleService) {}
+
+  ngOnInit(): void {
+    this.retrieveArticles();
+  }
+
+  retrieveArticles(): void {
+    this.articleService.getAll()
+      .subscribe({
+        next: (data) => {
+          this.articles = data;
+          console.log(data);
+        },
+        error: (e) => console.error(e)
+      });
+  }
 }
