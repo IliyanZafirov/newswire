7a4aecccb875688a78a160757d76e2256de7ae80
Iliyan Zafirov <iliyanzafirov00@gmail.com>
2024-03-08 16:48:33 +0200

    feat: implement article post

--- client/newswire/src/app/app.routes.ts
+++ client/newswire/src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 import { MainContainerComponent } from './main-container/main-container.component';
 import { ArticleDetailsComponent } from './article-details/article-details.component';
 import { AppComponent } from './app.component';
+import { ArticlePostComponent } from './article-post/article-post.component';
 
 export const routes: Routes = [
   { path: '',   redirectTo: '/articles', pathMatch: 'full' },
   { path: 'articles', component: MainContainerComponent },
+  { path: 'create-article', component:ArticlePostComponent },
   { path: 'articles/:id', component: ArticleDetailsComponent },
 ];
--- client/newswire/src/app/article-post/article-post.component.ts
+++ client/newswire/src/app/article-post/article-post.component.ts
@@ -1,21 +1,31 @@
 import { Component } from '@angular/core';
 import { ArticleService } from '../article-service/article.service';
 import { Router } from '@angular/router';
-import { Article } from '../model/article.model';
+import { Article} from '../model/article.model';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-article-post',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './article-post.component.html',
   styleUrl: './article-post.component.css'
 })
 export class ArticlePostComponent {
   article: Article=<Article>{};
-
+  
   constructor(private articleService: ArticleService, private router: Router) {}
 
+  articleForm = new FormGroup ({
+    title: new FormControl('',  {nonNullable: true}),
+    content: new FormControl('',  {nonNullable: true}),
+    category: new FormControl('',  {nonNullable: true}),
+    publicationDate: new FormControl('',  {nonNullable: true}),
+    imageURL: new FormControl('',  {nonNullable: true})
+  });
+
   onSubmit() {
+    this.article = this.articleForm.value as Article;
     this.articleService.post(this.article).subscribe({
       next: () => {
         this.router.navigate(["/"]);
@@ -25,4 +35,5 @@ export class ArticlePostComponent {
       }
     });
   }
+  
 }
